602559b56aed33551892fae3bfa3b26d55b0b1d3
kshayes78 <89166628+kshayes78@users.noreply.github.com>
2022-01-13 20:25:09 -0500

    commit tree d5e960ff854de5218c00350e2631f6560560a99f

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching (drivers, name) {
+          const result = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+          return result; 
+      }
+
+
+      function fuzzyMatch(drivers, firstLetters){
+          const result = drivers.filter(driver => driver.startsWith(firstLetters));
+          return result; 
+      }
+
+      function matchName(drivers, string){
+          const result = drivers.filter(driver => string === driver.name);
+            return result; 
+      }
+
+
+      
